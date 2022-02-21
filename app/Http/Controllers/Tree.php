790424f45469fc1c7ec4021740899cb653be10d3
Shinobi 3D <strelok0493@gmail.com>
2022-02-21 10:54:38 +0500

    static

--- app/Http/Controllers/Tree.php
+++ app/Http/Controllers/Tree.php
@@ -3,7 +3,7 @@ namespace App\Http\Controllers;
 
 class Tree extends Controller
 {
-   public function manzarali(){
+   static function manzarali(){
       return ['archa', 'qayragoch', 'gujum' , 'safna' ,'akatsiya', 'yasin'];
    }
 }
